65085cc7133bd64b2d91866d8a75edfb5244b35d
Swapnil Ghone <swapnilghone9@gmail.com>
2024-12-06 00:08:10 +0530

    fix:error handling while mongoose connection

--- server/db.js
+++ server/db.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URL;
 
 const connectToMongo = () =>{
-    mongoose.connect(mongoURI);
+    mongoose.connect(mongoURI).catch(err => console.log(err));
 } 
 
 mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on()
 
 module.exports =  connectToMongo;
